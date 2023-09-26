2da762c139474f627b4f8de15cbd4499d2c468de
sundayonah <sundayonah94@gmail.com>
2023-09-26 19:05:26 +0100

    add: icons

--- src/pages/investment.js
+++ src/pages/investment.js
@@ -5,7 +5,7 @@ const investment = () => {
       <>
          <main className="flex flex-col md:flex-row justify-evenly items-center m-6">
             {/* left side */}
-            <div className="w-full md:w-[40%] text-center p-4 border border-gray-500 rounded-lg shadow-md mb-6 md:mb-0">
+            <div className="w-full md:w-[40%] text-center p-4 border border-gray-500 rounded-lg shadow-md mt-6">
                <div className="flex justify-between items-center mb-4 ">
                   <span>Daily ROI Amount</span>
                   <span className="text-xs">0.00 BUSD</span>
@@ -14,7 +14,24 @@ const investment = () => {
                   <button className="w-full border border-[#BF9221] bg-gold-500 text-white px-4 py-2 rounded-lg hover:bg-[#BF9221] hover:text-white hover:border-transparent">
                      Mine Now
                   </button>
-                  <span className="flex pt-4">Next Mine Time: 00:00:00</span>
+                  <p className="flex pt-4 space-x-2">
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6  text-[#BF9221]"
+                     >
+                        <path
+                           stroke-linecap="round"
+                           stroke-linejoin="round"
+                           d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                     </svg>
+
+                     <span className="">Next Mine Time: 00:00:00</span>
+                  </p>
                </div>
                <div className="mt-10">
                   <div className="flex justify-between items-center mb-4  ">
@@ -24,14 +41,32 @@ const investment = () => {
                   <button className="w-full border border-[#BF9221] bg-gold-500 text-white px-4 py-2 rounded-lg bg-[#BF9221] hover:text-white hover:border-transparent">
                      Withdraw Profit
                   </button>
-                  <p className="mt-4">
-                     Next Withdraw Time: 00:00:00 (only 50% of profit allowance)
+
+                  <p className="flex mt-6 space-x-2">
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6  text-[#BF9221]"
+                     >
+                        <path
+                           stroke-linecap="round"
+                           stroke-linejoin="round"
+                           d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                     </svg>
+                     <span className="">
+                        Next Withdraw Time: 00:00:00 (only 50% of profit
+                        allowance)
+                     </span>
                   </p>
                </div>
             </div>
 
             {/* right side */}
-            <div className="w-full md:w-[40%] text-center p-4 mt-6 border border-gray-500 rounded-lg shadow-md">
+            <div className="w-full md:w-[40%] text-center p-4 border border-gray-500 rounded-lg shadow-md mt-6">
                <div className="flex justify-between items-center mb-4 ">
                   <span>Wallet Balance</span>
                   <span className="text-xs">0.00 BUSD</span>
--- src/pages/referalMenu.js
+++ src/pages/referalMenu.js
@@ -29,7 +29,23 @@ const ReferalMenu = () => {
                </div>
                <span className="flex justify-end mt-10 text-[#4169E1] cursor-pointer">
                   {/* Referral Link */}
-                  <button onClick={toggleModal}>Referral Link</button>
+                  <p className="flex pt-4 space-x-2">
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-5 h-5"
+                     >
+                        <path
+                           stroke-linecap="round"
+                           stroke-linejoin="round"
+                           d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                        />
+                     </svg>
+                     <button onClick={toggleModal}>Referral Link</button>
+                  </p>
                </span>
             </div>
          </main>
